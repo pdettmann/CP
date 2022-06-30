700a044ba77847e51b998c4aa1183193127844d7
Paula Dettmann <paula.dettmann@gmail.com>
2022-06-30 23:38:39 +0200

    update api url

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ function_calls = words[0]
 total_time = words[4]
 
 # Post profiler data to API
-api_url = '5q2hk4toq1.execute-api.us-west-2.amazonaws.com'
+api_url = 'https://api.ammonite-profiler.xyz'
 api_key = ENV.get('INPUT_API_KEY')
 
 if api_key == None:
@@ -40,7 +40,7 @@ else:
     body = {'functionCalls': function_calls, 'totalTime': total_time}
     json_body = json.dumps(body)
 
-    connection.request('POST', '/prod/SaveBenchmarkData', json_body, headers)
+    connection.request('POST', '/SaveBenchmarkData', json_body, headers)
 
     response = connection.getresponse()
     decoded_response = response.read().decode()
