a1aedb056ada963f855c15f55ea1629d39acd735
Paula Dettmann <paula.dettmann@gmail.com>
2022-07-04 21:36:41 +0200

    update utils import path in main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import os
-from utils import run_profiler, send_data
+from utils.util import run_profiler, send_data
 
 def main():
     try:
         api_key = os.environ.get('INPUT_API_KEY')
-        entry_file = os.environ.get("INPUT_ENTRY_FILE")
+        entry_file = os.environ.get('INPUT_ENTRY_FILE')
 
         profile_data = run_profiler("/github/workspace/" + entry_file)
         send_data(profile_data, api_key)
